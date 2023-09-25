a6b140bfb6f92b4f0299c7afcc6256da5dc53cb1
developedbygeo <georgiosanagnostou@hotmail.com>
2023-09-25 09:59:42 +0300

    feat: adding routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { CoursesComponent } from './courses/courses.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'courses', component: CoursesComponent },
+  { path: '**', redirectTo: '/home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
